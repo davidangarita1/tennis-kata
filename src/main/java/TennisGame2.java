cecbf4c0680a7d54f37f25b89b601ce6cea313c3
davidangarita1 <davidangarita.sistemas@gmail.com>
2021-12-12 20:05:35 -0500

    Se refactorizan el score diferente

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -3,9 +3,6 @@ public class TennisGame2 implements TennisGame
 {
     public int p1Point = 0;
     public int p2Point = 0;
-    
-    public String p1Res = "";
-    public String p2Res = "";
     private String player1Name;
     private String player2Name;
 
@@ -22,55 +19,7 @@ public class TennisGame2 implements TennisGame
         else if (p1Point >= 4 ||p2Point >= 4)
             score = scoreGreaterThan4();
 
-        if (p1Point > 0 && p2Point ==0)
-        {
-            if (p1Point ==1)
-                p1Res = "Fifteen";
-            if (p1Point ==2)
-                p1Res = "Thirty";
-            if (p1Point ==3)
-                p1Res = "Forty";
-            
-            p2Res = "Love";
-            score = p1Res + "-" + p2Res;
-        }
-        if (p2Point > 0 && p1Point ==0)
-        {
-            if (p2Point ==1)
-                p2Res = "Fifteen";
-            if (p2Point ==2)
-                p2Res = "Thirty";
-            if (p2Point ==3)
-                p2Res = "Forty";
-            
-            p1Res = "Love";
-            score = p1Res + "-" + p2Res;
-        }
-        
-        if (p1Point > p2Point && p1Point < 4)
-        {
-            if (p1Point ==2)
-                p1Res ="Thirty";
-            if (p1Point ==3)
-                p1Res ="Forty";
-            if (p2Point ==1)
-                p2Res ="Fifteen";
-            if (p2Point ==2)
-                p2Res ="Thirty";
-            score = p1Res + "-" + p2Res;
-        }
-        if (p2Point > p1Point && p2Point < 4)
-        {
-            if (p2Point ==2)
-                p2Res ="Thirty";
-            if (p2Point ==3)
-                p2Res ="Forty";
-            if (p1Point ==1)
-                p1Res ="Fifteen";
-            if (p1Point ==2)
-                p1Res ="Thirty";
-            score = p1Res + "-" + p2Res;
-        }
+        else score = differentScore(p1Point) + "-" + differentScore(p2Point);
 
         return score;
     }
