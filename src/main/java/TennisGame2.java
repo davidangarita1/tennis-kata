ea300c9aa0a068c1d40c7969b527066ce8ba73da
davidangarita1 <davidangarita.sistemas@gmail.com>
2021-12-12 20:08:25 -0500

    Se refactorizan las variables a cosntantes

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,10 +1,10 @@
 
 public class TennisGame2 implements TennisGame
 {
-    public int p1Point = 0;
-    public int p2Point = 0;
-    private String player1Name;
-    private String player2Name;
+    private int p1Point = 0;
+    private int p2Point = 0;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,7 +12,7 @@ public class TennisGame2 implements TennisGame
     }
 
     public String getScore(){
-        String score = "";
+        String score;
         if (p1Point == p2Point)
             score = equalScore();
 
