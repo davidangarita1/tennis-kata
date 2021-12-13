5c27933f961bd170e7cb021337873b8c90576773
davidangarita1 <davidangarita.sistemas@gmail.com>
2021-12-12 20:29:38 -0500

    Se refactorizan if menor a 4 por funcion

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,6 +1,6 @@
 
 public class TennisGame3 implements TennisGame {
-    
+
     private int socrePlayer2;
     private int scorePlayer1;
     private final String player1Name;
@@ -13,24 +13,38 @@ public class TennisGame3 implements TennisGame {
 
     public String getScore() {
         String s;
-        if (scorePlayer1 < 4 && socrePlayer2 < 4 && !(scorePlayer1 + socrePlayer2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[scorePlayer1];
-            return (scorePlayer1 == socrePlayer2) ? s + "-All" : s + "-" + p[socrePlayer2];
+        if (scoreIsLowerThan4()) {
+            return lowerThan4();
+        } else if (scorePlayer1 == socrePlayer2) {
+            return "Deuce";
         } else {
-            if (scorePlayer1 == socrePlayer2)
-                return "Deuce";
-            s = scorePlayer1 > socrePlayer2 ? player1Name : player2Name;
-            return ((scorePlayer1 - socrePlayer2)*(scorePlayer1 - socrePlayer2) == 1) ? "Advantage " + s : "Win for " + s;
+            return greaterThan4();
         }
     }
-    
+
+    private boolean scoreIsLowerThan4() {
+        return scorePlayer1 < 4 && socrePlayer2 < 4 && scorePlayer1 + socrePlayer2 != 6;
+    }
+
+    private String greaterThan4() {
+        String s;
+        s = scorePlayer1 > socrePlayer2 ? player1Name : player2Name;
+        return ((scorePlayer1 - socrePlayer2) * (scorePlayer1 - socrePlayer2) == 1) ? "Advantage " + s : "Win for " + s;
+    }
+
+    private String lowerThan4() {
+        String s;
+        String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        s = p[scorePlayer1];
+        return (scorePlayer1 == socrePlayer2) ? s + "-All" : s + "-" + p[socrePlayer2];
+    }
+
     public void wonPoint(String playerName) {
         if (playerName.equals(this.player1Name))
             this.scorePlayer1++;
         else
             this.socrePlayer2++;
-        
+
     }
 
 }
