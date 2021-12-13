8b8b5b344677852312b7fd7efaf7df146c61792d
davidangarita1 <davidangarita.sistemas@gmail.com>
2021-12-12 19:55:58 -0500

    Se refactorizan el mayores de 4 por funcion greatherThan4

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -16,7 +16,11 @@ public class TennisGame2 implements TennisGame
 
     public String getScore(){
         String score = "";
-        if (p1Point == p2Point) score = equalScore();
+        if (p1Point == p2Point)
+            score = equalScore();
+
+        else if (p1Point >= 4 ||p2Point >= 4)
+            score = scoreGreaterThan4();
 
         if (p1Point > 0 && p2Point ==0)
         {
@@ -67,26 +71,18 @@ public class TennisGame2 implements TennisGame
                 p1Res ="Thirty";
             score = p1Res + "-" + p2Res;
         }
-        
-        if (p1Point > p2Point && p2Point >= 3)
-        {
-            score = "Advantage player1";
-        }
-        
-        if (p2Point > p1Point && p1Point >= 3)
-        {
-            score = "Advantage player2";
-        }
-        
-        if (p1Point >=4 && p2Point >=0 && (p1Point - p2Point)>=2)
-        {
-            score = "Win for player1";
-        }
-        if (p2Point >=4 && p1Point >=0 && (p2Point - p1Point)>=2)
-        {
-            score = "Win for player2";
-        }
+
         return score;
+    }
+
+    private String differentScore(int scorePlayer) {
+        switch(scorePlayer)
+        {
+            case 0: return "Love";
+            case 1: return "Fifteen";
+            case 2: return "Thirty";
+            default: return "Forty";
+        }
     }
 
     private String equalScore() {
@@ -96,6 +92,22 @@ public class TennisGame2 implements TennisGame
             case 2: return "Thirty-All";
             default: return "Deuce";
         }
+    }
+
+    private String scoreGreaterThan4() {
+        int minusResult = p1Point - p2Point;
+        switch(minusResult){
+            case 1: case -1: return advantageScore(minusResult);
+            default: return winScore(minusResult);
+        }
+    }
+
+    private String advantageScore(int minusResult){
+        return minusResult == 1 ? "Advantage player1" : "Advantage player2";
+    }
+
+    private String winScore(int minusResult){
+        return minusResult >= 2 ? "Win for player1": "Win for player2";
     }
 
 
