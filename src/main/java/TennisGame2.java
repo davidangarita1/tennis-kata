f6f2523278a76d1d3f8139f5f1cb69cc2eba0c79
David H. Angarita <davidangarita.sistemas@gmail.com>
2021-12-15 21:55:15 -0500

    Se baja la complejidad de TennisGame2

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -13,29 +13,28 @@ public class TennisGame2 implements TennisGame
 
     public String getScore(){
         String score;
-        if (p1Point == p2Point)
+        if (isScoreEqual())
             score = equalScore();
 
-        else if (p1Point >= 4 ||p2Point >= 4)
+        else if (isScoreGreaterThan4())
             score = scoreGreaterThan4();
 
-        else score = differentScore(p1Point) + "-" + differentScore(p2Point);
+        else
+            score = differentScore(p1Point).concat("-").concat(differentScore(p2Point));
 
         return score;
     }
 
-    private String differentScore(int scorePlayer) {
-        switch(scorePlayer)
-        {
-            case 0: return "Love";
-            case 1: return "Fifteen";
-            case 2: return "Thirty";
-            default: return "Forty";
-        }
+    private boolean isScoreGreaterThan4() {
+        return p1Point >= 4 || p2Point >=4;
+    }
+
+    private boolean isScoreEqual() {
+        return p1Point == p2Point;
     }
 
     private String equalScore() {
-        switch (p1Point){
+        switch(p1Point){
             case 0: return "Love-All";
             case 1: return "Fifteen-All";
             case 2: return "Thirty-All";
@@ -52,11 +51,21 @@ public class TennisGame2 implements TennisGame
     }
 
     private String advantageScore(int minusResult){
-        return minusResult == 1 ? "Advantage player1" : "Advantage player2";
+        return minusResult ==1 ? "Advantage ".concat(player1Name) : "Advantage ".concat(player2Name);
     }
 
     private String winScore(int minusResult){
-        return minusResult >= 2 ? "Win for player1": "Win for player2";
+        return minusResult >= 2 ? "Win for ".concat(player1Name): "Win for ".concat(player2Name);
+    }
+
+    private String differentScore(int scorePlayer) {
+        switch(scorePlayer)
+        {
+            case 0: return "Love";
+            case 1: return "Fifteen";
+            case 2: return "Thirty";
+            default: return "Forty";
+        }
     }
 
     public void wonPoint(String player) {
